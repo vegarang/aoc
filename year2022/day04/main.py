9520065eac3a3a9b358adeb733ecad7233580ed6
Vegard Angell <v.angell123@gmail.com>
2022-12-04 22:28:32 +0100

    add main files for day4

---
+++ year2022/day04/main.py
@@ -0,0 +1,18 @@
+from year2022.day04.part1 import run_part_1
+from year2022.day04.part2 import run_part_2
+
+
+def load_file():
+    with open('input.txt', 'r') as f:
+        values = f.readlines()
+    return values
+
+
+def do_things():
+    values = load_file()
+    run_part_1(values)
+    run_part_2(values)
+
+
+if __name__ == '__main__':
+    do_things()
